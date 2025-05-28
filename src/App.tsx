e097e4eb7421f57b7b7de751a1994ee8e986aaf6
cupid <yang760241116@163.com>
2025-05-28 15:01:35 +0800

    init

---
+++ src/App.tsx
@@ -0,0 +1,51 @@
+import React, { useState, useEffect } from "react";
+
+const App: React.FC = () => {
+	const [themeArr] = useState([
+		{
+			name: "light",
+			type: "light",
+		},
+		{
+			name: "dark",
+			type: "dark",
+		},
+		{
+			name: "warm",
+			type: "warm",
+		},
+	]);
+	const [theme, setTheme] = useState(() => {
+		const storedTheme = sessionStorage.getItem("theme");
+		if (storedTheme) return storedTheme;
+		const prefersDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+		return prefersDark ? "dark" : "light";
+	});
+
+	useEffect(() => {
+		document.documentElement.setAttribute("data-theme", theme);
+		sessionStorage.setItem("theme", theme);
+	}, [theme]);
+	const toggleTheme = (theme: string) => {
+		setTheme(theme);
+		sessionStorage.setItem("theme", theme);
+	};
+	return (
+		<div className="text-cyan-50 h-screen bg-canvas">
+			<div>tailwind-css-test</div>
+			{themeArr.map((item, index) => (
+				<button
+					key={index}
+					onClick={() => {
+						toggleTheme(item.type);
+					}}
+					className="w-16 m-4 bg-cyan-600 h-10 rounded-lg"
+				>
+					{item.name}
+				</button>
+			))}
+		</div>
+	);
+};
+
+export default React.memo(App);
